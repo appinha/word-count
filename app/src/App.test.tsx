e9daebc6bb4bb80a608835c68da817e2d0ae77c2
appinha <amanda_pinha@hotmail.com>
2023-02-11 13:58:34 -0300

    Adds testing

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,49 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  const renderComponent = () => render(<App />)
+
+  test('renders properly', () => {
+    const { container } = renderComponent();
+
+    expect(container).toHaveTextContent(/Word Counter/i);
+    expect(container).toHaveTextContent(/Type something below/i);
+    expect(container).toHaveTextContent(/COUNT WORDS/i);
+  });
+
+  test('counts words', () => {
+    const { container } = renderComponent();
+
+    const textArea = screen.getByRole("textbox");
+    fireEvent.change(textArea, { target: {value: "Lorem ipsum"} });
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+    expect(container).toHaveTextContent(/Total of 2 words/i);
+  });
+
+  test('counts single word', () => {
+    const { container } = renderComponent();
+
+    const textArea = screen.getByRole("textbox");
+    fireEvent.change(textArea, { target: {value: "Lorem"} });
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+    expect(container).toHaveTextContent(/Total of 1 word/i);
+  });
+
+  test('displays instructions when clicking button with no text', () => {
+    const { container } = renderComponent();
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+    expect(container).toHaveTextContent(/Please enter some text/i);
+    expect(container).not.toHaveTextContent(/Total of (.*) word/i);
+  });
 });
